a54790bc23e913d92c8b74288d8b3c2eef4e6598
OlgaShalaginova <olechkatsapova@gmail.com>
2022-11-26 13:19:57 +0500

    Добавили Решение

--- Program.cs
+++ Program.cs
@@ -34,11 +34,13 @@ void PrintArray(string[] array)
 
 void FillSecondArrayWithIF(string[] array1, string[] array2)
 {
+    int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
       if(array1[i].Length <= 3)
          {
-          array2[i] = array1[i];
+          array2[count] = array1[i];
+          count++;
          }
     }
 }
